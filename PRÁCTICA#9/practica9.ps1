afe7f5e4cdf3898037e8d0a0e8caec2883456805
Andrea P. Arzate <88362609+andreaarz@users.noreply.github.com>
2021-11-10 04:19:44 -0600

    Add files via upload

---
+++ PRÁCTICA#9/practica9.ps1
@@ -0,0 +1,5 @@
+﻿
+$DNS =  ipconfig /displaydns 
+$ruta = "C:\Users\andre\Documents\result_practica9.txt"
+$cod = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($DNS))
+Set-Content -Value "$cod" -Path $ruta
